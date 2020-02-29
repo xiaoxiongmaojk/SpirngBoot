2421ad39439d5d80021ba12fce0c8082836f0bd0
yang_jiaping <yang_jiaping@ymsl.com.cn>
2020-02-29 09:36:41 +0800

    first commit spring boot demo

---
+++ src/main/java/com/lu/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.lu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class UserController {
+    @RequestMapping("getUser")
+    @ResponseBody
+    public String getUser(){
+        return "Spring hello";
+    }
+}
